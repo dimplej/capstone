3b32ce83788836201cdec26655b1022c3cca5372
Tejasvi Jain <dinal.tejasvi@gmail.com>
2025-06-12 17:35:23 +0530

    Added the about-us page

---
+++ blocks/about-us/about-us.js
@@ -0,0 +1,44 @@
+
+
+
+/*export default function decorate(block) {
+  const contributors = [
+    {
+      name: 'Stacey Roswells',
+      roles: 'ARTIST | PHOTOGRAPHER | TRAVELER',
+      img: '/path-to-images/stacey.jpg',
+    },
+    {
+      name: 'Jake Hammer',
+      roles: 'INFLUENCER | WRITER',
+      img: '/path-to-images/jake.jpg',
+    },
+    {
+      name: 'Ian Provo',
+      roles: 'PHOTOGRAPHER',
+      img: '/path-to-images/ian.jpg',
+    },
+    {
+      name: 'Jacob Wester',
+      roles: 'SKATER | WRITER',
+      img: '/path-to-images/jacob.jpg',
+    },
+  ];
+
+  const container = document.createElement('div');
+  container.className = 'contributor-list';
+
+  contributors.forEach((c) => {
+    const el = document.createElement('div');
+    el.className = 'contributor';
+    el.innerHTML = `
+      <img src="${c.img}" alt="${c.name}">
+      <h3>${c.name}</h3>
+      <p>${c.roles}</p>
+    `;
+    container.appendChild(el);
+  });
+
+  block.innerHTML = '';
+  block.appendChild(container);
+}*/
